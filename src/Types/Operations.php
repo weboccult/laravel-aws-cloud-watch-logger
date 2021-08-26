61b36e5d5d0866478f97a2ba7aae87fff2261ad5
Darshit <dev005.weboccult@gmail.com>
2021-08-26 17:36:53 +0530

    New updates - operations sorted in alphabetical order - modules sorted in alphabetical order

--- src/Types/Operations.php
+++ src/Types/Operations.php
@@ -7,25 +7,25 @@ namespace Weboccult\LaravelAwsCloudWatchLogger\Types;
  */
 class Operations
 {
-    public const TEST = 'Test';
-    public const ANY = 'Any';
-    public const OTHER = 'Other';
-    public const GENERAL = 'General';
-    public const COMMON = 'Common';
     public const ANONYMOUS = 'Anonymous';
+    public const ANY = 'Any';
+    public const COMMON = 'Common';
+    public const GENERAL = 'General';
+    public const OTHER = 'Other';
+    public const TEST = 'Test';
 
-    public const CREATE = 'Create';
-    public const UPDATE = 'Update';
-    public const DELETE = 'Delete';
-    public const EDIT = 'Edit';
-    public const VIEW = 'View';
     public const BACKUP = 'Backup';
+    public const CREATE = 'Create';
+    public const CREATE_ROW = 'Create-Row';
+    public const CREATE_TABLE = 'Create-Table';
+    public const DELETE = 'Delete';
+    public const DROP_ROW = 'Drop-Row';
+    public const DROP_TABLE = 'Drop-Table';
+    public const EDIT = 'Edit';
     public const EXTRACT = 'Extract';
     public const FORCE_DELETE = 'Force-Delete';
     public const GENERAL_MIGRATION = 'General-Migration';
-    public const CREATE_ROW = 'Create-Row';
     public const MODIFY_ROW = 'Modify-Row';
-    public const DROP_ROW = 'Drop-Row';
-    public const CREATE_TABLE = 'Create-Table';
-    public const DROP_TABLE = 'Drop-Table';
+    public const UPDATE = 'Update';
+    public const VIEW = 'View';
 }
